c3c1d0d0b5eb6d1e7c8cd0053ac4c3a6dbf95494
rolodato <rlopezdato@frba.utn.edu.ar>
2013-10-20 21:10:57 -0300

    falto DROP SCHEMA

---
+++ data/borrar_schema.sql
@@ -0,0 +1,20 @@
+-- Esto NO es parte de la entrega, solo es para testear el script
+DROP TABLE mario_killers.Afiliado
+DROP TABLE mario_killers.Atencion
+DROP TABLE mario_killers.Bono_Consulta
+DROP TABLE mario_killers.Bono_Farmacia
+DROP TABLE mario_killers.Especialidad
+DROP TABLE mario_killers.Especialidad_Profesional
+DROP TABLE mario_killers.Compra
+DROP TABLE mario_killers.Medicamento
+DROP TABLE mario_killers.Medicamento_Receta
+DROP TABLE mario_killers.Persona
+DROP TABLE mario_killers.Plan_Med
+DROP TABLE mario_killers.Profesional
+DROP TABLE mario_killers.Receta
+DROP TABLE mario_killers.Sintoma
+DROP TABLE mario_killers.Tipo_Especialidad
+DROP TABLE mario_killers.Turno
+DROP TABLE mario_killers.Usuario
+
+DROP SCHEMA mario_killers

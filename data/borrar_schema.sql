e64596c10674a16b77896e79092d600cc50a856b
Tomas Gropper <tomasgropper@gmail.com>
2013-10-25 15:02:42 -0300

    constraints de agenda, faltan fechas pisadas

--- data/borrar_schema.sql
+++ data/borrar_schema.sql
@@ -1,6 +1,7 @@
 -- Esto NO es parte de la entrega, solo es para testear el script
 DROP TABLE mario_killers.Afiliado
 DROP TABLE mario_killers.Atencion
+DROP TABLE mario_killers.Agenda
 DROP TABLE mario_killers.Bono_Consulta
 DROP TABLE mario_killers.Bono_Farmacia
 DROP TABLE mario_killers.Especialidad
@@ -12,6 +13,7 @@ DROP TABLE mario_killers.Persona
 DROP TABLE mario_killers.Plan_Medico
 DROP TABLE mario_killers.Profesional
 DROP TABLE mario_killers.Receta
+DROP TABLE mario_killers.Rango
 DROP TABLE mario_killers.Sintoma
 DROP TABLE mario_killers.Tipo_Especialidad
 DROP TABLE mario_killers.Turno
@@ -19,5 +21,7 @@ DROP TABLE mario_killers.Usuario
 DROP TABLE mario_killers.Tipo_Documento
 DROP TABLE mario_killers.Estado_Civil
 DROP TABLE mario_killers.Grupo_Familia
+
+DROP FUNCTION mario_killers.horas_por_semana
 
 DROP SCHEMA mario_killers
--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -18,13 +18,13 @@ CREATE TABLE Persona (
 	PRIMARY KEY (id)
 	-- FOREIGN KEY (tipo_doc) REFERENCES Tipo_Documento(id)
 	-- FOREIGN KEY (usuario) REFERENCES Usuario(id)
-)
+) GO
 
 CREATE TABLE Tipo_Documento (
 	id int IDENTITY,
 	tipo varchar(10),
 	PRIMARY KEY (id)
-)	
+) GO
 
 CREATE TABLE Usuario (
 	nombre varchar(255),
@@ -34,7 +34,7 @@ CREATE TABLE Usuario (
 	activo bit,
 	PRIMARY KEY (nombre),
 	-- FOREIGN KEY (persona) REFERENCES Persona(id)
-) 
+) GO
 
 CREATE TABLE Plan_Medico (
 	codigo numeric(18, 0),
@@ -42,7 +42,7 @@ CREATE TABLE Plan_Medico (
 	precio_bono_consulta numeric(18, 0),
 	precio_bono_farmacia numeric(18, 0),
 	PRIMARY KEY (codigo)
-)
+) GO
 
 CREATE TABLE Compra (
 	id int IDENTITY,
@@ -50,7 +50,7 @@ CREATE TABLE Compra (
 	persona int,
 	plan_medico int,
 	PRIMARY KEY (id)
-)
+) GO
 
 CREATE TABLE Afiliado (
 	persona int,
@@ -64,40 +64,82 @@ CREATE TABLE Afiliado (
 	-- FOREIGN KEY (persona) REFERENCES Persona(id)
 	-- FOREIGN KEY (estado_civil) REFERENCES Estado_Civil(id)
 	-- FOREIGN KEY (grupo_familia) REFERENCES Grupo_Familia(codigo)
-)
+) GO
 
 CREATE TABLE Estado_Civil (
 	id int IDENTITY,
 	estado varchar(255),
 	PRIMARY KEY (id)
-)
+) GO
 
 CREATE TABLE Grupo_Familia (
 	codigo numeric(18, 0),
 	plan_medico numeric(18, 0),
 	PRIMARY KEY (codigo),
 	-- FOREIGN KEY (plan_medico) REFERENCES Plan_Med(codigo)
-)
+) GO
 
 CREATE TABLE Profesional (
 	persona int,
 	matricula int,
 	activo bit,
 	PRIMARY KEY (persona),
+	-- FOREIGN KEY (persona) REFERENCES Persona(id)
 	-- UNIQUE (matricula)
-)
+) GO
+
+CREATE TABLE Agenda (
+	profesional int,
+	desde date NOT NULL,
+	hasta date NOT NULL,
+	PRIMARY KEY (profesional),
+	CONSTRAINT max_120_dias CHECK (DATEDIFF(day, desde, hasta) < 120),
+	CONSTRAINT fechas_validas CHECK (desde < hasta)
+	-- FOREIGN KEY (profesional) REFERENCES Profesional(persona)
+) GO
+
+CREATE FUNCTION mario_killers.horas_por_semana(@profesional int) RETURNS int AS
+BEGIN
+	RETURN (SELECT SUM(DATEDIFF(HOUR, hora_desde, hora_hasta))
+	FROM mario_killers.Rango
+	WHERE Rango.profesional = @profesional)
+END
+GO
+
+CREATE TABLE Rango (
+	dia int, -- domingo = 1, valor default de DATEFIRST
+	profesional int,
+	hora_desde time,
+	hora_hasta time,
+	CONSTRAINT horarios_validos CHECK (
+	CASE
+		WHEN dia BETWEEN 2 AND 6
+		     AND hora_desde BETWEEN '07:00:00' AND '20:00:00'
+		     AND hora_hasta BETWEEN '07:00:00' AND '20:00:00'
+		THEN 1
+		WHEN dia = 7
+		     AND hora_desde BETWEEN '10:00:00' AND '15:00:00'
+		     AND hora_hasta BETWEEN '10:00:00' AND '15:00:00'
+		THEN 1
+	END = 1
+	AND hora_desde < hora_hasta
+	),
+	CONSTRAINT max_horas_por_semana CHECK (mario_killers.horas_por_semana(profesional) <= 48),
+	-- FOREIGN KEY (profesional) REFERENCES Profesional(persona)
+	-- TODO: Ver fechas que se pisen
+) GO
 
 CREATE TABLE Especialidad_Profesional (
 	profesional int,
 	especialidad int,
 	PRIMARY KEY (profesional, especialidad)
-)
+) GO
 
 CREATE TABLE Tipo_Especialidad (
 	codigo numeric(18, 0) IDENTITY,
 	descripcion varchar(255),
 	PRIMARY KEY (codigo)
-)
+) GO
 
 CREATE TABLE Especialidad (
 	codigo numeric(18, 0) IDENTITY,
@@ -105,7 +147,7 @@ CREATE TABLE Especialidad (
 	tipo numeric(18, 0),
 	PRIMARY KEY (codigo),
 	-- FOREIGN KEY (tipo) REFERENCES Tipo_Especialidad(codigo)
-)
+) GO
 
 CREATE TABLE Turno (
 	id int IDENTITY,
@@ -119,7 +161,7 @@ CREATE TABLE Turno (
 	-- FOREIGN KEY (persona) REFERENCES Persona(id),
 	-- FOREIGN KEY (profesional) REFERENCES Profesional(persona),
 	-- FOREIGN KEY (especialidad) REFERENCES Especialidad(codigo)
-)
+) GO
 
 CREATE TABLE Atencion (
 	turno int,
@@ -127,7 +169,7 @@ CREATE TABLE Atencion (
 	diagnostico varchar(255),
 	PRIMARY KEY (turno),
 	-- FOREIGN KEY (turno) REFERENCES Turno(id)
-)
+) GO
 
 CREATE TABLE Bono_Consulta (
 	id int IDENTITY,
@@ -139,7 +181,7 @@ CREATE TABLE Bono_Consulta (
 	-- FOREIGN KEY (plan_medico) REFERENCES Plan_Medico(codigo),
 	-- FOREIGN KEY (compra) REFERENCES Compra(id),
 	-- FOREIGN KEY (turno) REFERENCES Turno(id)
-)
+) GO
 
 CREATE TABLE Bono_Farmacia (
 	id int,
@@ -150,36 +192,36 @@ CREATE TABLE Bono_Farmacia (
 	-- FOREIGN KEY (plan_medico) REFERENCES Plan_Medico(codigo),
 	-- FOREIGN KEY (compra) REFERENCES Compra(id),
 	-- FOREIGN KEY (receta) REFERENCES Receta(id)
-)
+) GO
 
 CREATE TABLE Medicamento_Receta (
 	medicamento int,
 	receta int,
-	cantidad int, -- constraint  <= 3
+	cantidad int,
 	PRIMARY KEY (medicamento, receta),
 	-- FOREIGN KEY (medicamento) REFERENCES Medicamento(id),
 	-- FOREIGN KEY (receta) REFERENCES Receta(id)
-)
+	CONSTRAINT max_3 CHECK (cantidad <= 3)
+)GO
 
 CREATE TABLE Medicamento (
 	id int IDENTITY,
 	detalle varchar(255),
 	PRIMARY KEY (id)
-)
+) GO
 
 CREATE TABLE Receta (
 	id int IDENTITY,
 	atencion int,
 	PRIMARY KEY (id),
 	-- FOREIGN KEY (atencion) REFERENCES Atencion(turno)
-)
+) GO
 
 CREATE TABLE Sintoma (
 	atencion int,
 	detalle varchar(255),
 	PRIMARY KEY (atencion),
 	-- FOREIGN KEY (atencion) REFERENCES Atencion(turno)
-)
+) GO
 
 --------------------------------- MIGRACION ---------------------------------
-

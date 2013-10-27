9dc2f6be1c8eeebd32ab8a5d2fa6278de2137dce
rolodato <rlopezdato@frba.utn.edu.ar>
2013-10-27 17:10:07 -0300

    funcion roles por usuario

---
+++ sql/roles_usuario.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION roles_usuario(@username varchar(255))
+RETURNS @roles TABLE (rol int, nombre varchar(255)) AS
+BEGIN
+	INSERT INTO @roles
+		SELECT id, nombre
+		FROM
+			mario_killers.Roles_Usuario JOIN mario_killers.Rol
+			ON Roles_Usuario.rol = Rol.id
+		WHERE Roles_Usuario.usuario = @username
+	RETURN
+END
+GO

83be99baff5b62080d040c92bfdca8fe72c98282
Uriel <petauriel@gmail.com>
2013-10-27 12:23:21 -0300

    algunos cambios

---
+++ src/Clinica Frba/Clases/Roles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+
+namespace Clinica_Frba.Clases
+{
+    class Roles
+    {
+        public static List<Rol> ObtenerRoles(string filtro)
+        {
+            List<Rol> listaDeRoles = new List<Rol>();
+
+            List<OleDbParameter> ListaParametros = new List<OleDbParameter>();
+            ListaParametros.Add(new OleDbParameter("@txt", filtro));
+
+            OleDbConnection conexion = Clases.BaseDeDatos.ObtenerConexion();
+            OleDbDataReader lector = Clases.BaseDeDatos.ObtenerDataReader("", "T", ListaParametros);
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    //FALTA TRAER LAS FUNCIONALIDADES POR ROL
+                    Rol unRol = new Rol();
+                    unRol.Id = (int)lector ["id"];
+                    unRol.Nombre = (string)lector["nombre"];
+                    unRol.Habilitado = true;
+                    listaDeRoles.Add(unRol);
+                }
+            }
+            return listaDeRoles;
+        }
+    }
+}
---
+++ src/Clinica Frba/Clases/Utiles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clinica_Frba.Clases
+{
+    class Utiles
+    {
+        public static bool EsNumerico(string numero)
+        {
+            try
+            {
+                int test = Convert.ToInt32(numero);
+                return true;
+            }
+            catch (Exception)
+            {return false;}
+        }
+
+        public static bool EsFechaValida(DateTime fecha)
+        {
+            try { 
+                DateTime hoy = DateTime.Today;
+                if (fecha > hoy)
+                {
+                    return false;
+                } return true;
+            }
+            catch { return false; }
+        }
+
+    public enum Operacion
+    {
+        Baja,
+        Modificacion,
+        Seleccion
+    }
+    }
+}

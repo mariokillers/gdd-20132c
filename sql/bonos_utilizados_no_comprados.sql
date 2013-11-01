58a647e287666b7f74f0914443142cb88ea0815e
rolodato <rlopezdato@frba.utn.edu.ar>
2013-11-01 19:42:38 -0300

    listado estadistico 3, REVISAR porque da 0

---
+++ sql/bonos_utilizados_no_comprados.sql
@@ -0,0 +1,13 @@
+SELECT (SELECT COUNT(Bono_Consulta.id)
+FROM mario_killers.Bono_Consulta
+     JOIN mario_killers.Compra ON Bono_Consulta.compra = Compra.id
+     JOIN mario_killers.Turno ON Bono_Consulta.turno = Turno.id
+WHERE Compra.persona <> Turno.persona)
+
++
+
+(SELECT COUNT(Bono_Farmacia.id)
+FROM mario_killers.Bono_Farmacia
+     JOIN mario_killers.Compra ON Bono_Farmacia.compra = Compra.id
+     JOIN mario_killers.Turno ON Bono_Farmacia.turno = Turno.id
+WHERE Compra.persona <> Turno.persona)

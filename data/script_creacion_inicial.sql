2d6adc87751f1cbc6877b27c52237ff9991a91c0
Uriel <petauriel@gmail.com>
2013-11-01 16:53:00 -0300

    faltaba funcion de roles de un usuario

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -36,6 +36,19 @@ BEGIN
 END
 GO
 
+CREATE FUNCTION mario_killers.roles_usuario(@username varchar(255))
+RETURNS @roles TABLE (rol int, nombre varchar(255)) AS
+BEGIN
+	INSERT INTO @roles
+		SELECT id, nombre
+		FROM
+			mario_killers.Rol_Usuario JOIN mario_killers.Rol
+			ON Rol_Usuario.rol = Rol.id
+		WHERE Rol_Usuario.usuario = @username
+	RETURN
+END
+GO
+
 CREATE PROCEDURE mario_killers.agregar_funcionalidad(@rol varchar(255), @func varchar(255)) AS
 BEGIN
 	INSERT INTO mario_killers.Funcionalidad_Rol (rol, funcionalidad)

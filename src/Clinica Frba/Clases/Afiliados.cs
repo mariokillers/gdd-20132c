a156747ce4d33ab5f8df033bea84e85b01196a81
Tomas Gropper <tomasgropper@gmail.com>
2013-11-01 18:13:31 -0300

    avance en ABM afiliado

--- src/Clinica Frba/Clases/Afiliados.cs
+++ src/Clinica Frba/Clases/Afiliados.cs
@@ -11,24 +11,62 @@ namespace Clinica_Frba.Clases
 {
     class Afiliados
     {
-        public static List<Afiliado> ObtenerAfiliados()
+        public static List<Afiliado> ObtenerAfiliados(String nombre, String apellido, String dni, String numeroAfiliado, decimal codigoPlan)
         {
             List<Afiliado> Lista = new List<Afiliado>();
 
+            String auxAfil = numeroAfiliado.ToString();
+
+
             List<SqlParameter> ListaParametros = new List<SqlParameter>();
-            SqlDataReader lector = Clases.BaseDeDatosSQL.ObtenerDataReader("", "T", ListaParametros);
+            ListaParametros.Add(new SqlParameter ("@nombre", "%"+nombre+"%"));
+            ListaParametros.Add(new SqlParameter("@apellido", "%"+apellido+"%"));
+            ListaParametros.Add(new SqlParameter("@dni", dni));
+            ListaParametros.Add(new SqlParameter("@numeroAfiliado", auxAfil.Remove(auxAfil.Length - 2)));
+            ListaParametros.Add(new SqlParameter("@codigoPlan", codigoPlan));
+
+            SqlDataReader lector = Clases.BaseDeDatosSQL.ObtenerDataReader("SELECT grupo_familia, nro_familiar, apellido, nombre, documento, plan_familiar FROM mario_killers.AfiliadosABM WHERE grupo_familia *100 + nro_familiar = @numeroAfiliado OR apellido LIKE @apellido OR nombre LIKE @nombre OR documento = @dni OR plan_familiar = @codigoPlan", "T", ListaParametros);
 
             if (lector.HasRows)
             {
                 while (lector.Read())
                 {
                     Afiliado unAfiliado = new Afiliado();
-                    //unAfiliado.Codigo = 0;//me lo devuelve la consulta
-                    //unAfiliado.Descripcion = (string)lector["desripcion"];
+                    unAfiliado.Apellido = (string)lector["apellido"];
+                    unAfiliado.Nombre = (string)lector["nombre"];
+                    unAfiliado.Numero_Grupo = (decimal)lector["grupo_familia"];
+                    unAfiliado.Numero_Familiar = (int)lector["nro_familiar"];
+                    unAfiliado.Documento = (decimal)lector["documento"];
+                    unAfiliado.Plan_Medico = (decimal)lector["plan_medico"];
                     Lista.Add(unAfiliado);
                 }
             }
             return Lista;
+        }
+
+        public static List<Afiliado> ObtenerTodos()
+        {
+            List<Afiliado> listaDeAfiliados = new List<Afiliado>();
+
+            List<SqlParameter> ListaParametros = new List<SqlParameter>();
+
+            SqlDataReader lector = Clases.BaseDeDatosSQL.ObtenerDataReader("SELECT * FROM mario_killers.AfiliadosABM", "T", ListaParametros);
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    Afiliado unAfiliado = new Afiliado();
+                    unAfiliado.Apellido = (string)lector["apellido"];
+                    unAfiliado.Nombre = (string)lector["nombre"];
+                    unAfiliado.Numero_Grupo = (decimal)lector["grupo_familia"];
+                    unAfiliado.Numero_Familiar = (int)lector["nro_familiar"];
+                    unAfiliado.Documento = (decimal)lector["documento"];
+                    unAfiliado.Plan_Medico = (decimal)lector["plan_medico"];
+                    listaDeAfiliados.Add(unAfiliado);
+                }
+            }
+            return listaDeAfiliados; ;
         }
     }
 }

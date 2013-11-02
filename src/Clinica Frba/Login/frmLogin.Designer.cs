0e621d05be0263c8053b91b3848b54a10860a1a2
Tomas Gropper <tomasgropper@gmail.com>
2013-11-02 20:36:27 -0300

    agrego el user y pass por defecto que me canse de escribirlos jaja

--- src/Clinica Frba/Login/frmLogin.Designer.cs
+++ src/Clinica Frba/Login/frmLogin.Designer.cs
@@ -69,6 +69,7 @@
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(216, 20);
             this.txtPassword.TabIndex = 6;
+            this.txtPassword.Text = "w23e";
             this.txtPassword.UseSystemPasswordChar = true;
             // 
             // txtUserName
@@ -77,6 +78,7 @@
             this.txtUserName.Name = "txtUserName";
             this.txtUserName.Size = new System.Drawing.Size(216, 20);
             this.txtUserName.TabIndex = 5;
+            this.txtUserName.Text = "admin";
             // 
             // frmLogin
             // 

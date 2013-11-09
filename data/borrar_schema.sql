c1b76fac8c6627a3c98556d194053a00e684df5a
rolodato <rlopezdato@frba.utn.edu.ar>
2013-11-09 18:44:01 -0300

    arreglado script de borrar

--- data/borrar_schema.sql
+++ data/borrar_schema.sql
@@ -1,4 +1,6 @@
 -- Esto NO es parte de la entrega, solo es para testear el script
+DROP VIEW mario_killers.AfiliadosABM, mario_killers.ProfesionalYPersona, mario_killers.ProfesionalABM
+
 DROP TABLE
 	mario_killers.Medicamento_Turno,
     mario_killers.Medicamento,
@@ -13,16 +15,16 @@ DROP TABLE
     mario_killers.Profesional,
     mario_killers.Modificaciones_Grupo,
     mario_killers.Bajas_Afiliado,
-    mario_killers.Afiliado,
-    mario_killers.Grupo_Familia,
-    mario_killers.Estado_Civil,
-    mario_killers.Compra,
-    mario_killers.Plan_Medico,
     mario_killers.Rol_Usuario,
     mario_killers.Funcionalidad_Rol,
     mario_killers.Funcionalidad,
     mario_killers.Rol,
     mario_killers.Usuario,
+    mario_killers.Compra,
+    mario_killers.Afiliado,
+    mario_killers.Grupo_Familia,
+    mario_killers.Estado_Civil,
+    mario_killers.Plan_Medico,
     mario_killers.Persona,
     mario_killers.Tipo_Documento
            
@@ -40,7 +42,5 @@ DROP PROCEDURE mario_killers.agregar_funcionalidad,
                mario_killers.registrarCambioPlan,
                mario_killers.hacerCompra,
                mario_killers.agregarProfesional
-
-DROP VIEW mario_killers.AfiliadosABM, mario_killers.ProfesionalYPersona, mario_killers.ProfesionalABM
 
 DROP SCHEMA mario_killers

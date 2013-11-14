3c7c0d10a38a6074318264893a22699d562ea85e
Uriel <petauriel@gmail.com>
2013-11-14 14:53:44 -0300

    Agrego algo que me interesa

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -651,10 +651,8 @@ FROM mario_killers.Afiliado A JOIN mario_killers.Persona P ON A.persona = P.id
 GO
 
 CREATE VIEW mario_killers.BonoYcompra AS
-SELECT c.id AS compra, c.fecha AS fecha, bf.codigo AS codigo, bf.plan_medico AS plan_medico
+SELECT c.id AS compra, c.fecha AS fecha, bf.codigo AS codigo, bf.plan_medico AS plan_medico, a.grupo_familia AS grupo
 FROM mario_killers.Bono_Farmacia bf
 	join mario_killers.Compra c on c.id = bf.compra
+	join mario_killers.Afiliado a on a.persona = c.persona
 GO
-
-
-select * from mario_killers.BonoYcompra
--- sql/Vistas ABM/BonoPorUsuario.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW mario_killers.BonoYcompra AS
-SELECT c.id AS compra, c.fecha AS fecha, bf.codigo AS codigo, bf.plan_medico AS plan_medico
-FROM mario_killers.Bono_Farmacia bf
-	join mario_killers.Compra c on c.id = bf.compra
-GO
-
-
-select * from mario_killers.BonoYcompra

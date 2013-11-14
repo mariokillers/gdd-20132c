bc5d4122bda30e1e0bd7ffa4619d1759d9e1f623
Uriel <petauriel@gmail.com>
2013-11-14 14:13:08 -0300

    Agrego algo que me interesa

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -651,7 +651,7 @@ FROM mario_killers.Afiliado A JOIN mario_killers.Persona P ON A.persona = P.id
 GO
 
 CREATE VIEW mario_killers.BonoPorUsuario AS
-SELECT a.grupo_familia
+SELECT a.grupo_familia, bf.codigo
 FROM mario_killers.Bono_Farmacia bf
 	JOIN mario_killers.Compra c ON c.id = bf.compra
 	JOIN mario_killers.Persona p ON p.id = c.persona
--- sql/Vistas ABM/BonoPorUsuario.sql
+++ sql/Vistas ABM/BonoPorUsuario.sql
@@ -1,5 +1,5 @@
 CREATE VIEW mario_killers.BonoPorUsuario AS
-SELECT a.grupo_familia
+SELECT a.grupo_familia, bf.codigo
 FROM mario_killers.Bono_Farmacia bf
 	JOIN mario_killers.Compra c ON c.id = bf.compra
 	JOIN mario_killers.Persona p ON p.id = c.persona

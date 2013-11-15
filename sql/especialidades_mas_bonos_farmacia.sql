0b56fe3118d81d748edd1ff00c2238404035548a
rolodato <rlopezdato@frba.utn.edu.ar>
2013-11-15 00:00:42 -0300

    query bonos farmacias especialidad funcionando de nuevo

--- sql/especialidades_mas_bonos_farmacia.sql
+++ sql/especialidades_mas_bonos_farmacia.sql
@@ -1,7 +1,6 @@
-SELECT TOP 5 Especialidad.descripcion AS especialidad, COUNT(Bono_Farmacia.codigo) AS cantidad
-FROM mario_killers.Bono_Farmacia
-     JOIN mario_killers.Turno ON Bono_Farmacia.turno = Turno.id
-     JOIN mario_killers.Especialidad ON Turno.especialidad = Especialidad.codigo
+SELECT TOP 5 Especialidad.descripcion AS 'Especialidad', COUNT(DISTINCT bono_farmacia) AS 'Bonos recetados'
+FROM mario_killers.Medicamento_HistoriaClinica
+	JOIN mario_killers.Historia_Clinica ON Medicamento_HistoriaClinica.historia_clinica = Historia_Clinica.id
+     JOIN mario_killers.Especialidad ON Historia_Clinica.especialidad = Especialidad.codigo
 GROUP BY Especialidad.descripcion
-ORDER BY COUNT(Bono_Farmacia.codigo) DESC
-
+ORDER BY COUNT(DISTINCT bono_farmacia) DESC

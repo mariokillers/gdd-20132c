c9207ef4f06e94c6ef7961b799f4386fbda4917e
rolodato <rlopezdato@frba.utn.edu.ar>
2013-11-15 02:52:43 -0300

    preparar entrega zip

--- sql/Vistas ABM/BonoYcompra.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW mario_killers.BonoYcompra AS
-SELECT c.id AS compra, c.fecha AS fecha, bf.codigo AS codigo, bf.plan_medico AS plan_medico, a.grupo_familia AS grupo
-FROM mario_killers.Bono_Farmacia bf
-	join mario_killers.Compra c on c.id = bf.compra
-	join mario_killers.Afiliado a on a.persona = c.persona
-GO
-
-
-select * from mario_killers.BonoYcompra
--- sql/Vistas ABM/ProfesionalPersona.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW mario_killers.ProfesionalYPersona AS
-SELECT pro.matricula AS matricula, per.apellido AS apellido, per.nombre AS nombre, per.id AS codigoPersona, per.direccion AS direccion, per.documento AS documento, per.fecha_nac AS fechaNac, per.mail AS mail, per.sexo AS sexo, per.telefono AS tel, per.tipo_doc AS tipo_doc
-FROM mario_killers.Profesional pro JOIN mario_killers.Persona per ON pro.persona = per.id
-GO 
-
